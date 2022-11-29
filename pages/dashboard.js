091d05fdaba059e7273f16842f4c255662388514
praizoh <oremei.akande@gmail.com>
2022-11-29 10:12:45 +0000

    add dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,214 @@
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
+import { client, explorePublications } from "../api";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    try {
+      const response = await client.query(explorePublications).toPromise();
+      console.log({ response });
+      setPosts(response.data.publications.items);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <div className="dasboard">
+      <Head>
+        <title>Lit dashboard</title>
+        <meta name="description" content="Lit-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        {/* nav bar */}
+        <div className="nav">
+          <img
+            src="images/profile-picture.png"
+            alt="profile"
+            width="32px"
+            height="32px"
+          />
+          <img src="" alt="logo" />
+        </div>
+        {/* <!-- story section --> */}
+        <div id="story">
+          <div>
+            <div>
+              <img
+                src="images/add-story.png"
+                alt=""
+                width="56px"
+                height="56px"
+              />
+            </div>
+            <p>Add Story</p>
+          </div>
+          <div>
+            <div>
+              <img
+                src="images/profile-picture.png"
+                className="status"
+                alt=""
+                width="56px"
+                height="56px"
+              />
+            </div>
+            <p>My Story</p>
+          </div>
+          <div>
+            <div>
+              <img
+                src="images/girl1.png"
+                className="status"
+                alt="cherish"
+                width="56px"
+                height="56px"
+              />
+            </div>
+            <p>Cherish.eth</p>
+          </div>
+          <div>
+            <div>
+              <img
+                src="images/skeleton.png"
+                className="status"
+                alt=""
+                width="56px"
+                height="56px"
+              />
+            </div>
+            <p>ntfnow</p>
+          </div>
+          <div>
+            <div>
+              <img
+                src="images/mike.png"
+                className="status"
+                alt=""
+                width="56px"
+                height="56px"
+              />
+            </div>
+            <p>Mikes Adams</p>
+          </div>
+          <div>
+            <div>
+              <img
+                src="images/cordon.png"
+                className="status"
+                alt=""
+                width="56px"
+                height="56px"
+              />
+            </div>
+            <p>Cordon</p>
+          </div>
+        </div>
+        {/* <!-- posts section --> */}
+        {posts.map((post) => (
+          <div id="web3">
+            <div className="webl">
+              <div>
+                {/* { post. */}
+                <img
+                  src="images/web3logo.png"
+                  alt=""
+                  className="status"
+                  width="30px"
+                  height="30px"
+                />
+                {/* // } */}
+              </div>
+              <p>{post.profile.name}</p>
+              <div className="dots">
+                <div></div>
+              </div>
+            </div>
+            {/* <!-- <div className="posts"> --> */}
+            <img
+              src="images/web3-polygon.png"
+              className="posts"
+              alt=""
+              width="426px"
+              height="300px"
+            />
+            {/* <video >
+              <source src={post.metadata.media[0].original.url} type="video/mp4" control width="426px"
+              height="300px"/>
+            </video> */}
+            {/* <!-- </div> --> */}
+            <div className="interact">
+              <div className="like">
+                <img
+                  src="images/like.png"
+                  alt="like"
+                  width="20.8px"
+                  height="20px"
+                />
+                <img
+                  src="images/comments.png"
+                  alt="comment"
+                  width="20px"
+                  height="20px"
+                />
+                <img
+                  src="images/share.png"
+                  alt="share"
+                  width="20px"
+                  height="20px"
+                />
+              </div>
+              <div className="dots">
+                <div className="dot" id="active"></div>
+                <div className="dot"></div>
+              </div>
+              <div>
+                <img src="images/save.png" alt="" width="15px" height="20px" />
+              </div>
+            </div>
+            <div className="liked">
+              <div>
+                <img
+                  src="images/liked-by.png"
+                  alt=""
+                  width="44px"
+                  height="20px"
+                />
+              </div>
+              <p>Liked by Zainab and 60 others</p>
+            </div>
+            <div className="content">
+              <p className="web3cont">{post.profile.name}</p>
+              <p>
+                {post.metadata.content}
+                {/* <span>...Read more</span> */}
+              </p>
+            </div>
+            <div className="comments">
+              <div className="comment">
+                <img
+                  src="images/profile-picture.png"
+                  alt="profile"
+                  width="20px"
+                  height="20px"
+                />
+                <p>Add comments</p>
+              </div>
+              <p className="view">
+                View all {post.stats.totalAmountOfComments} comments
+              </p>
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,162 +1,61 @@
-import { Contract, providers, utils } from "ethers";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect, useEnsAvatar,
+  useEnsName, } from "wagmi";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
-import Web3Modal from "web3modal";
+import Link from "next/link";
+import Router from 'next/router'
 
 export default function Home() {
-  // walletConnected keep track of whether the user's wallet is connected or not
-  const [walletConnected, setWalletConnected] = useState(false);
-  // ENS
-  const [ens, setENS] = useState("");
-  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
-  const web3ModalRef = useRef();
-  // Save the address of the currently connected account
-  const [address, setAddress] = useState("");
+  const { data: account } = useAccount();
+  const [ address, setAddress ] = useState("");
+  // const { data: ensAvatar } = useEnsAvatar({ address })
+  // const { data: ensName } = useEnsName({ address })
+  // const { disconnect } = useDisconnect();
+  const [ens, setENS] = useState(""); 
 
-  /**
-   * Sets the ENS, if the current connected address has an associated ENS or else it sets
-   * the address of the connected account
-   */
-  const setENSOrAddress = async (address, web3Provider) => {
-    // Lookup the ENS related to the given address
-    var _ens = await web3Provider.lookupAddress(address);
-    // If the address has an ENS set the ENS or else just set the address
-    if (_ens) {
-      setENS(_ens);
-    } else {
-      setAddress(address);
+  // const setENSOrAddress = async () => {
+  //   console.log(ensName)
+
+  // };
+
+  useEffect(()=>{
+    if(account){
+      if(account.address){
+        setAddress(account.address) // account.address="89494", address=89494
+      }
     }
-  };
-
-  /*
-      connectWallet: Connects the MetaMask wallet
-    */
-  const connectWallet = async () => {
-    try {
-      // Get the provider from web3Modal, which in our case is MetaMask
-      // When used for the first time, it prompts the user to connect their wallet
-      await getProviderOrSigner(true);
-      setWalletConnected(true);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  /**
-   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
-   * signing capabilities of metamask attached
-   *
-   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
-   *
-   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
-   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
-   * request signatures from the user using Signer functions.
-   *
-   * @param {*} needSigner - True if you need the signer, default false otherwise
-   */
-  const getProviderOrSigner = async (needSigner = false) => {
-    // Connect to Metamask
-    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider);
-
-    // If user is not connected to the Goerli network, let them know and throw an error
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
-    }
-
-    if (needSigner) {
-      const signer = web3Provider.getSigner();
-      // Get the address associated to the signer which is connected to  MetaMask
-      const address = await signer.getAddress();
-      // Calls the function to set the ENS or Address
-      await setENSOrAddress(address, web3Provider);
-      return signer;
-    }
-    return web3Provider;
-  };
+  },[account])
 
   /*
       renderButton: Returns a button based on the state of the dapp
     */
   const renderButton = () => {
     // If wallet is not connected, return a button which allows them to connect their wllet
-    if (!walletConnected) {
+    if (!address) {
       return (
         <div>
-          <div class="connect-msg">
-            <p class="welcome"> Welcome Back To Lit</p>
-            <p class="connect-acct">
+          <div className="connect-msg">
+            <p className="welcome"> Welcome Back To Lit</p>
+            <p className="connect-acct">
               Connect your account to get started with Lit
             </p>
           </div>
-          <div class="connect-wallet" onClick={connectWallet}>
+          <div className="connect-wallet">
             <img src="./images/wallet.png" alt="" />
-            <span>connect to wallet</span>
+            <div><ConnectButton
+              label="Connect to wallet"
+              style="background-color: transparent; color: black;"
+            /></div>
           </div>
-          <p class="text-center">or use</p>
-          <div class="icon-container">
-            <div class="icons">
-              <img
-                src="./images/key.png"
-                alt=""
-                width="25px"
-                class="image-png"
-              />
-            </div>
-
-            <div class="icons">
-              <img
-                src="./images/ion_finger-print.png"
-                alt=""
-                width="25px"
-                class="image-png png1"
-              />
-            </div>
-          </div>
-          <div class="keys">
-            <p>ID Key</p>
-            <p> Fingerprint</p>
-          </div>
-
-          <button class="btn-login" onClick={connectWallet}> Log in</button>
         </div>
       );
     } else {
-      return (
-        <div>
-          
-          <div class="connect-wallet">
-            <img src="./images/wallet.png" alt="" />
-            <h3>You are connected as {ens?ens:address}!</h3>
-          </div>
-        </div>
-      );
-    }
-
-    // If we are currently waiting for something, return a loading button
-    if (loading) {
-      return <button className={styles.button}>Loading...</button>;
+      Router.push('/dashboard')
     }
   };
-
-  useEffect(() => {
-    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
-    if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
-      // The `current` value is persisted throughout as long as this page is open
-      web3ModalRef.current = new Web3Modal({
-        network: "goerli",
-        providerOptions: {},
-        disableInjectedProvider: false,
-      });
-      connectWallet();
-    }
-  }, [walletConnected]);
   return (
-    <div>
+    <div className="login">
       <Head>
         <title>Lit</title>
         <meta name="description" content="Lit-Dapp" />
